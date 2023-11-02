05d9529f26e4d9423d6c7bc7cd0d748cc9119a2e
Ezekiel <ezekieljamolin186@gmail.com>
2023-11-01 18:46:11 -0700

    added columns and finished work for all the files in models folder

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
+Category.belongsTo(Product, {
+  foreignKey: 'product_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
